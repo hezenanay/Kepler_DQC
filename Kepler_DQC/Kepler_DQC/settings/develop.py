b941840db42551a7f10f7a5ad6bc58f75d0a5ca0
Zenan He <540059517@qq.com>
2019-12-10 17:15:01 +0800

    数据库连接由sqlite3迁至MySQL

--- Kepler_DQC/Kepler_DQC/settings/develop.py
+++ Kepler_DQC/Kepler_DQC/settings/develop.py
@@ -7,7 +7,14 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', #数据库引擎
+        'NAME': 'test',                       #数据库名
+        'USER': 'root',                       #用户名
+        'PASSWORD': 'HZN60213991d',           #密码
+        'HOST': '',                           #数据库主机，默认为localhost
+        'PORT': '',                           #数据库端口，MySQL默认为3306
+        'OPTIONS': {
+            'autocommit': True,
+         },
     }
 }
