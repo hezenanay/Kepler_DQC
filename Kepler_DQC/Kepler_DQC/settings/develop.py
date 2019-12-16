cdcdb5d28855809f6db4c4a923018f5f0e985f93
Zenan He <540059517@qq.com>
2019-12-16 18:54:11 +0800

    新建了用户实例和对应用户表，新增了后台管理系统超级用户，修改了MySQL连接库

--- Kepler_DQC/Kepler_DQC/settings/develop.py
+++ Kepler_DQC/Kepler_DQC/settings/develop.py
@@ -8,7 +8,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', #数据库引擎
-        'NAME': 'test',                       #数据库名
+        'NAME': 'Kepler_DQC',                 #数据库名
         'USER': 'root',                       #用户名
         'PASSWORD': 'HZN60213991d',           #密码
         'HOST': '',                           #数据库主机，默认为localhost
--- Kepler_DQC/login/models.py
+++ Kepler_DQC/login/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+    gender_kv=(
+        ('male','男'),
+        ('female','女'),
+    )
+
+    username=models.CharField(max_length=128, unique=True)
+    password=models.CharField(max_length=255)
+    email=models.CharField(max_length=128, unique=True)
+    gender=models.CharField(max_length=32, choices=gender_kv, default='男')
+    phone=models.CharField(max_length=32, unique=True)
+    c_time=models.DateTimeField(auto_now_add=True)
+    u_time=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering=['-c_time']
+        verbose_name='用户'
+        verbose_name_plural='用户'
