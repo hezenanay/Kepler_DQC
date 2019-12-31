8f9e8270768bbbdc22c5db9cc01079561fa73899
Zenan He <540059517@qq.com>
2019-12-31 15:35:14 +0800

    新增了登录判断、新建异常监控、新建阈值监控的view函数

--- Kepler_DQC/error_ctrl/views.py
+++ Kepler_DQC/error_ctrl/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+import hashlib
+from .forms import *
+from .models import *
+
 
 # Create your views here.
+
+def new_error_ctrl(request):
+    if not request.session.get('is_login', None):
+        # 发现没有登录则强制跳转到登录页面
+        return redirect('/login/')
+    if request.method == 'POST':
+        error_form = ErrorctrlForm(request.POST)
+        if error_form.is_valid():
+            cleaned_data = error_form.cleaned_data
+            c_type = cleaned_data['c_type']
+            s_id = cleaned_data['s_id']
+            server_add = cleaned_data['server_add']
+            t_name = cleaned_data['t_name']
+            c_name = cleaned_data['c_name']
+            condition = cleaned_data['condition']
+            time_option = cleaned_data['time_option']
+            r_time = cleaned_data['r_time']
+            o_user_email = cleaned_data['o_user_email']
+            p_user_email = cleaned_data['p_user_email']
+            if c_type == None or r_time == None or o_user_email == None or p_user_email == None:
+                message = '请检查必填项是否完整！'
+                return render(request, 'error_ctrl/new_error_ctrl.html', locals())
+            if c_type == 'delay' and s_id == None:
+                message = '请检查必填项是否完整！'
+                return render(request, 'error_ctrl/new_error_ctrl.html', locals())
+            if c_type in ['equal_zero', 'equal_null', 'equal_neg'] and (
+                    server_add == None or t_name == None or c_name == None):
+                message = '请检查必填项是否完整！'
+                return render(request, 'error_ctrl/new_error_ctrl.html', locals())
+
+            # 当一切ok的情况下，创建新任务
+
+            new_task = Error_task()
+            new_task.c_type = c_type
+            new_task.s_id = s_id
+            new_task.server_add = server_add
+            new_task.t_name = t_name
+            new_task.c_name = c_name
+            new_task.condition = condition
+            new_task.time_option = time_option
+            new_task.r_time = r_time
+            new_task.o_user_email = o_user_email
+            new_task.p_user_email = p_user_email
+            new_task.save()
+            return redirect('/error_ctrl_list/')  # 自动跳转到异常监控列表页面
+    error_form = ErrorctrlForm()
+    return render(request, 'error_ctrl/new_error_ctrl.html', locals())
--- Kepler_DQC/login/views.py
+++ Kepler_DQC/login/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .forms import *
+from .models import *
+
 
 # Create your views here.
+
+def login(request):
+    if request.session.get('is_login', None):
+        #发现已登录则强制跳转到主页面
+        return redirect('/index/')
+    if request.method == 'POST':
+        login_form = UserFrom(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            try:
+                user = User.objects.get(username=username)
+                if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_email'] = user.email
+                    return redirect('/index/')
+                else:
+                    message = '密码不正确！'
+            except:
+                message = '用户不存在！'
+        return render(request, 'login/login.html', locals())
+    login_form = UserFrom()
+    return render(request, 'login/login.html', locals())
